bc23366511159680b49f8413a613e3ed46c136f1
Kirk Lange <mail@kirklange.com>
2018-05-15 15:14:35 -0700

    Project sources now also in src

--- script/ezc.mk
+++ script/ezc.mk
@@ -32,13 +32,13 @@ BLD_DIR = $(ROOT)/build
 DOC_DIR = $(ROOT)/docs
 # External (git submodule) directory
 # Should be set by "includer" Makefile
-#EXT_DIR = $(ROOT)/ext
+#SUB_DIR = $(ROOT)/sub
 # Include
 INC_DIR = $(ROOT)/include
 # Libraries
 LIB_DIR = $(ROOT)/lib
-# Projects
-PRJ_DIR = $(ROOT)/project
+# Projects, examples, and tests
+PRJ_DIR = $(ROOT)/src
 # Resources (config, textures, etc)
 RES_DIR = $(ROOT)/res
 # Source
@@ -47,13 +47,14 @@ SRC_DIR = $(ROOT)/src
 
 
 # Add source directory to source file names
-OBJS = $(foreach OBJ,$(EXT_SRC_FILES),$(EXT_DIR)/$(OBJ)) \
-	   $(foreach DIR,$(EXT_SUBDIRS),$(wildcard $(EXT_DIR)/$(DIR)/*.c)) \
+OBJS = $(foreach OBJ,$(EXT_SRC_FILES),$(SUB_DIR)/$(OBJ)) \
+	   $(foreach DIR,$(EXT_SUBDIRS),$(wildcard $(SUB_DIR)/$(DIR)/*.c)) \
 	   $(foreach DIR,$(SRC_SUBDIRS),$(wildcard $(SRC_DIR)/$(DIR)/*.c)) \
-	   $(foreach DIR,$(PRJ_SUBDIR),$(wildcard $(PRJ_DIR)/$(DIR)/*.c)) \
+	   $(foreach DIR,$(MAIN_SUBDIR),$(wildcard $(PRJ_DIR)/$(DIR)/*.c)) \
 
 # Include and library flags
-INC = -I$(INC_DIR) $(foreach DIR,$(EXT_INC_DIRS),-I$(EXT_DIR)/$(DIR))
+INC = -I$(INC_DIR) $(foreach DIR,$(EXT_INC_DIRS),-I$(SUB_DIR)/$(DIR)) \
+	  -I$(PRJ_DIR)/$(MAIN_SUBDIR)
 LIB =
 
 # Find what OS we're on so we can better configure all the compiler options
@@ -99,8 +100,8 @@ MAKE = make --no-print-directory
 
 
 
-.PHONY : all $(BLD_DIR) run dirs deps $(EXT_DIR) $(DOC_DIR) rtd
-.PHONY : compile help clean clean-$(BLD_DIR) clean-$(EXT_DIR) open
+.PHONY : all $(BLD_DIR) run dirs deps $(SUB_DIR) $(DOC_DIR) rtd
+.PHONY : compile help clean clean-$(BLD_DIR) clean-$(SUB_DIR) open
 
 all :
 	$(MAKE) $(DOC_DIR)
@@ -113,16 +114,16 @@ help :
 	@echo
 
 open : # Usage example: `make open F=hello W=vs`
-	@$(EXT_DIR)/ezc/script/ezc_open.sh \
+	@$(SUB_DIR)/ezc/script/ezc_open.sh \
 		$(SRC_DIR) $(INC_DIR) $(PRJ_DIR) $(F) $(W)
 
 $(DOC_DIR) :
 	mkdir -p $(DOC_DIR)
 	$(MAKE) clean-$(DOC_DIR)
-	$(MAKE) $(EXT_DIR)
+	$(MAKE) $(SUB_DIR)
 	@# TODO: version check, m.css requires python 3.6+ and doxygen 1.8.14+
-	python3 $(EXT_DIR)/m.css/doxygen/dox2html5.py .doxyfile-mcss
-	cp $(EXT_DIR)/m.css/css/m-dark+doxygen.compiled.css \
+	python3 $(SUB_DIR)/m.css/doxygen/dox2html5.py .doxyfile-mcss
+	cp $(SUB_DIR)/m.css/css/m-dark+doxygen.compiled.css \
 		$(DOC_DIR)/m-dark+doxygen.compiled.css
 	find docs/m-dark+doxygen.compiled.css -type f -exec \
 		sed -i 's/text-indent: 1\.5rem/text-indent: 0rem/g' {} \;	
@@ -143,10 +144,10 @@ $(BLD_DIR) :
 	mkdir -p $(RES_DIR)
 	cp -R $(BIN_DIR)/. $(BLD_DIR)/$(OUT)/
 	cp -R $(RES_DIR) $(BLD_DIR)/$(OUT)/
-	$(MAKE) $(EXT_DIR)
+	$(MAKE) $(SUB_DIR)
 	$(MAKE) compile
 
-$(EXT_DIR) :
+$(SUB_DIR) :
 	git submodule init
 	git submodule update
 
@@ -164,7 +165,7 @@ clean :
 clean-all :
 	$(MAKE) clean-$(BLD_DIR)
 	$(MAKE) clean-$(DOC_DIR)
-	$(MAKE) clean-$(EXT_DIR)
+	$(MAKE) clean-$(SUB_DIR)
 
 clean-$(BLD_DIR) :
 	rm -rf $(BLD_DIR)/*
@@ -172,6 +173,6 @@ clean-$(BLD_DIR) :
 clean-$(DOC_DIR) :
 	rm -rf $(DOC_DIR)/*
 
-clean-$(EXT_DIR) :
-	rm -rf $(EXT_DIR)/*
+clean-$(SUB_DIR) :
+	rm -rf $(SUB_DIR)/*
 
